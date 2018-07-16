27263f9fea00dd2e72a230b14c939d938ea20153
Jordan Richmeier <raziell74@users.noreply.github.com>
2018-07-16 17:13:27 -0600

    Phpmig setup (#2)
    * initial phpmig setup
    * Fixed some pathing issues within docker

--- config/settings.php
+++ config/settings.php
@@ -3,7 +3,4 @@
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-$config['db']['host']   = $_ENV["MYSQL_ADDRESS"];
-$config['db']['user']   = $_ENV["MYSQL_USERNAME"];
-$config['db']['pass']   = $_ENV["MYSQL_PASSWORD"];
-$config['db']['dbname'] = $_ENV["MYSQL_DATABASE"];
+require 'db/dbconfig.php';
