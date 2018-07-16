65039b7baca909bf44dfe5d5fb35cd3791ab0873
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-07-16 16:48:50 -0600

    initial phpmig setup

---
+++ config/db/dbconfig.php
@@ -0,0 +1,5 @@
+<?php
+$config['db']['host']   = $_ENV["MYSQL_ADDRESS"];
+$config['db']['user']   = $_ENV["MYSQL_USERNAME"];
+$config['db']['pass']   = $_ENV["MYSQL_PASSWORD"];
+$config['db']['dbname'] = $_ENV["MYSQL_DATABASE"];
---
+++ config/phpmig.php
@@ -0,0 +1,29 @@
+<?php
+
+# phpmig.php
+
+use Phpmig\Adapter;
+use Pimple\Container;
+
+$container = new Container();
+
+$container['db'] = function () {
+    include "./db/dbconfig.php";
+    $db = $config['db'];
+    $pdo = new PDO(
+        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$container['phpmig.adapter'] = function ($c) {
+    return new Adapter\PDO\Sql($c['db'], 'migrations');
+};
+
+$container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'db' . DIRECTORY_SEPARATOR . 'migrations';
+$container['phpmig.migrations_template_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'db' . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . "migration_template.tmpl";
+
+return $container;
