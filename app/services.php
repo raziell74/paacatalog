c41f41779755cb7c4050776946af50184fdcb49c
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-07-16 16:19:13 -0600

    Added a services confirguration file

---
+++ app/services.php
@@ -0,0 +1,28 @@
+<?php
+$container = $app->getContainer();
+
+/**
+ * MonoLog Logging service
+ *
+ * @usage	$this->logger->addInfo("[LOGGED INFORMATION]");
+ */
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+/**
+ * Database Service
+ */
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO(
+        'mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+        $db['user'], $db['pass']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
--- public/index.php
+++ public/index.php
@@ -1,18 +1,11 @@
 <?php
 
 require '../vendor/autoload.php';
-
 require '../config/settings.php';
+
 $app = new \Slim\App(['settings' => $config]);
 
-$container = $app->getContainer();
-$container['logger'] = function($c) {
-    $logger = new \Monolog\Logger('my_logger');
-    $file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
-    $logger->pushHandler($file_handler);
-    return $logger;
-};
-
+require '../app/services.php';
 require '../app/routes.php';
 
 $app->run();
