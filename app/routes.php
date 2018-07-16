a1ef7e1e66c9c68f7ab123390559726a2e65fc68
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-07-16 15:58:27 -0600

    Set up basic slim functionality

---
+++ app/routes.php
@@ -0,0 +1,16 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("PAA Catalog Administration");
+
+    return $response;
+});
+
+$app->get('/sample', function (Request $request, Response $response, array $args) {
+    $sampleHtml = file_get_contents("catalog_sample.html");
+    $response->getBody()->write($sampleHtml);
+
+    return $response;
+});
