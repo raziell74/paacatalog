53374eac2191474de80cb6faa5cc33154a845412
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-08-17 10:40:59 -0600

    Localize CSS files

--- app/views/catalog.php
+++ app/views/catalog.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title>Peak Analysis & Automation - Catalog</title>
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <?php if($is_admin) { ?><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><?php } ?>
+    <style type="text/css"><?php include(__DIR__ . "/css/materialize.min.css"); ?></style>
     <style type="text/css"><?php include(__DIR__ . "/css/Style" . ($is_admin ? '' : '.min') . ".css"); ?></style>
 </head>
 <body>
