3139bb84fe941e0b6b24e6937184554b59995a2f
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-08-20 23:12:31 -0600

    Update view image helper so that it allows for image object processing

--- app/helpers/image_helper.php
+++ app/helpers/image_helper.php
@@ -1,10 +1,14 @@
 <?php
-function encoded_img($url, $is_admin = true) {
-    if($is_admin) return $url;
-    $file_info = new finfo(FILEINFO_MIME_TYPE);
-    $images_dir = strpos($url,"http") === false ? __DIR__ . "/../../public" : "";
-    $image = file_get_contents($images_dir . $url);
-    $mime_type = $file_info->buffer($image);
-    $image_data = base64_encode($image);
-    return "data:image/$mime_type;base64,$image_data";
+function encoded_img($image, $is_admin = true) {
+    if(is_object($image)) {
+        return $is_admin ? $image->url : $image->encoded_image;
+    } else {
+        if($is_admin) return $image;
+        $file_info = new finfo(FILEINFO_MIME_TYPE);
+        $images_dir = strpos($image,"http") === false ? __DIR__ . "/../../public" : "";
+        $image_file = file_get_contents($images_dir . $image);
+        $mime_type = $file_info->buffer($image_file);
+        $image_data = base64_encode($image_file);
+        return "data:image/$mime_type;base64,$image_data";
+    }
 }
