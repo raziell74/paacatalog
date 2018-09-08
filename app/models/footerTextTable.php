fde982e69e211365af4b073c21ba2a25ac23db23
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-09-07 21:10:31 -0600

    Add footer text table models

---
+++ app/models/footerTextTable.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Models;
+
+class footerTextTable extends Model {
+    public function getFooterText() {
+        $statement = $this->db->prepare("
+            SELECT
+                *
+            FROM
+                footer_text
+            WHERE
+                id = :footer_text_id
+        ");
+        $statement->bindParam(':footer_text_id', 1);
+        $statement->execute();
+        $footer_text = $this->fetchAsFooterTextObjs($statement->fetchAll());
+        return $footer_text[0] ?? null;
+    }
+
+    private function fetchAsFooterTextObjs($raw_footer_texts) {
+        $footer_texts = [];
+        foreach($raw_footer_texts as $footer_text_data) {
+            $footer_texts[] = new footerText($this->container, $footer_text_data);
+        }
+
+        return $footer_texts;
+    }
+}
