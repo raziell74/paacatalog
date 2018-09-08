6fc32813dc43d693b15ec01be4dd19c72ad2c799
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-09-07 20:57:18 -0600

    Hoisted coloring css classes in footer

--- app/views/catalog.php
+++ app/views/catalog.php
@@ -28,9 +28,9 @@
     <footer class="page-footer orange">
         <div class="container">
             <div class="row">
-                <div class="col l6 s12">
-                    <h5 class="white-text">Company Bio</h5>
-                    <p class="grey-text text-lighten-4">We Design and build a range of precision laboratory robots for laboratory and manufacturing applications. The founders of the company have many years’ experience in factory automation and robotics prior to incorporating in 1989. The company started out (under the name of MEKAnize Inc.) as an integrator of motion control products and then expanded into manufacturing its own robots. Currently the robots are manufactured in-house for use in the biotechnology and clinical automation markets. Our range includes the BiNEDx, KiNEDx, SciNEDx and GX robots.</p>
+                <div class="col l6 s12 white-text text-lighten-4">
+                    <h5>Company Bio</h5>
+                    <p>We Design and build a range of precision laboratory robots for laboratory and manufacturing applications. The founders of the company have many years’ experience in factory automation and robotics prior to incorporating in 1989. The company started out (under the name of MEKAnize Inc.) as an integrator of motion control products and then expanded into manufacturing its own robots. Currently the robots are manufactured in-house for use in the biotechnology and clinical automation markets. Our range includes the BiNEDx, KiNEDx, SciNEDx and GX robots.</p>
                 </div>
             </div>
         </div>
