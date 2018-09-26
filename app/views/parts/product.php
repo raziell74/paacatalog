6b5dd7feebe457fab8fb7190d08e98cfa3c7687e
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-09-26 12:35:28 -0600

    Moved product title into card footer

--- app/views/parts/product.php
+++ app/views/parts/product.php
@@ -2,15 +2,16 @@
     <div class="card">
         <div class="card-image center">
             <div class="embeded-image <?=$product->main_image->cssId?>"></div>
-            <span class="card-title"><?=$product->name?></span>
+            <?php if($view->isAdmin) { ?>
+                <span class="card-title">
+                    <a href="#<?=$section->cssId?>-product-list-anchor" class="btn-floating btn-large waves-effect waves-light blue product-edit" data-editor-id="#<?=$section->cssId?>-<?=$product->cssId?>-edit"><i class="material-icons">edit</i></a>
+                    <button data-product="<?=$product->id?>"class="btn-floating btn-large waves-effect waves-light red product-delete"><i class="material-icons">delete</i></button>
+                </span>
+            <?php } ?>
         </div>
         <div class="card-action">
-            <a class="btn-large waves-effect waves-light blue darken-4 modal-trigger" href="#<?=$product->cssId?>-modal">Check It Out</a>
-            <?php if($view->isAdmin) { ?>
-                <a href="#<?=$section->cssId?>-product-list-anchor" class="btn-floating btn-large waves-effect waves-light blue product-edit" data-editor-id="#<?=$section->cssId?>-<?=$product->cssId?>-edit"><i class="material-icons">edit</i></a>
-                <button data-product="<?=$product->id?>"class="btn-floating btn-large waves-effect waves-light red product-delete"><i class="material-icons">delete</i></button>
-            <?php } ?>
-
+            <span class="card-title"><?=$product->name?></span>
+            <a class="btn-large waves-effect waves-light blue darken-4 modal-trigger right" href="#<?=$product->cssId?>-modal" style="margin-top: -10px;">Check It Out</a>
             <?=$view->getPart('productModal', ['product' => $product])?>
         </div>
     </div>
