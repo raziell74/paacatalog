cefceaf1e3cce2b9156120c4e0e8ce9967259104
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-10-23 15:27:05 -0600

    Added some space at the top of the section edit form

--- app/views/parts/sectionAdminForm.php
+++ app/views/parts/sectionAdminForm.php
@@ -1,6 +1,6 @@
 <?php $savePath = $section->id == 0 ? '/add/section' : '/save/section/' . $section->id; ?>
 <form action="<?=$savePath?>" method="post" id="<?=$section->cssId?>-edit" enctype="multipart/form-data" class="z-depth-5 hide">
-    <div class="parallax-container valign-wrapper orange-text">
+    <div class="parallax-container valign-wrapper orange-text" style="padding: 20px 0px;">
         <div class="section no-pad-bot">
             <div class="container">
                 <div class="row center">
--- public/js/initAdmin.js
+++ public/js/initAdmin.js
@@ -7,7 +7,7 @@ var defaultMCESettings = {
         'searchreplace visualblocks code fullscreen',
         'insertdatetime media table contextmenu paste code help wordcount'
     ],
-    toolbar: 'insert | undo redo |  formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+    toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
     content_css: [
         '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
         '//www.tinymce.com/css/codepen.min.css'
@@ -18,5 +18,17 @@ function applyTinyMCE(settings){
     tinymce.init(settings);
     setTimeout(function(){
         $('.mce-widget.mce-notification.mce-notification-warning').remove();
-    }, 500);
+    }, 200);
+    setTimeout(function(){
+        $('.mce-widget.mce-notification.mce-notification-warning').remove();
+    }, 400);
+    setTimeout(function(){
+        $('.mce-widget.mce-notification.mce-notification-warning').remove();
+    }, 600);
+    setTimeout(function(){
+        $('.mce-widget.mce-notification.mce-notification-warning').remove();
+    }, 800);
+    setTimeout(function(){
+        $('.mce-widget.mce-notification.mce-notification-warning').remove();
+    }, 1000);
 }
