edf388d1b41f61ae4b197d96416f21dcdd7c514e
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-10-23 15:37:03 -0600

    Add swapping mechanics to sections table model

--- app/models/sectionsTable.php
+++ app/models/sectionsTable.php
@@ -3,7 +3,7 @@ namespace App\Models;
 
 class sectionsTable extends Model {
     public function getAll() {
-        $statement = $this->db->query("SELECT * FROM sections");
+        $statement = $this->db->query("SELECT * FROM sections ORDER BY sort_order ASC");
         $sections = $this->fetchAsSectionObjs($statement->fetchAll());
         return $sections;
     }
@@ -21,6 +21,53 @@ class sectionsTable extends Model {
         $statement->execute();
         $sections = $this->fetchAsSectionObjs($statement->fetchAll());
         return $sections[0] ?? null;
+    }
+
+    public function swapSectionOrder($firstSectionId, $secondSectionId) {
+        $statement = $this->db->prepare("
+            SELECT
+                id,
+                sort_order
+            FROM
+                sections
+            WHERE
+                id IN (:first_section_id, :second_section_id)
+            ORDER BY
+              sort_order ASC
+        ");
+        $statement->bindParam(':first_section_id', $firstSectionId);
+        $statement->bindParam(':second_section_id', $secondSectionId);
+        $statement->execute();
+
+        $sections = $statement->fetchAll();
+        $firstSectionNewSortOrder = $sections[1]['sort_order'];
+        $secondSectionNewSortOrder = $sections[0]['sort_order'];
+
+        $statement = $this->db->prepare("
+            UPDATE
+                sections
+            SET
+                sort_order=:first_new_sort_order
+            WHERE
+                id = :first_section_id
+        ");
+        $statement->bindParam(':first_section_id', $firstSectionId);
+        $statement->bindParam(':first_new_sort_order', $firstSectionNewSortOrder);
+        $statement->execute();
+
+        $statement = $this->db->prepare("
+            UPDATE
+                sections
+            SET
+                sort_order=:second_new_sort_order
+            WHERE
+                id = :second_section_id
+        ");
+        $statement->bindParam(':second_section_id', $secondSectionId);
+        $statement->bindParam(':second_new_sort_order', $secondSectionNewSortOrder);
+        $statement->execute();
+
+        return true;
     }
 
     public function addSection($data) {
